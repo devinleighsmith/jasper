51b72096e1cc2aa22826a53df7545e456e3b7109
Ronaldo Macapobre <rmacapob@gov.bc.ca>
2024-12-02 20:56:37 +0000

    - Migrate from vue-cli to vite - Created a Dockerfile.local to be able to create the web-dev image for local development - Fixes for codes that no longer works after vite migration - Disable splunk as there is a newer way of calling it (via REST API)

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,48 +1,46 @@
 import LoadingSpinner from "@components/LoadingSpinner.vue"
 import "@styles/index.scss"
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import 'core-js/stable'
-import 'intersection-observer'
-import 'regenerator-runtime/runtime'
-import Vue from 'vue'
-import VueResource from 'vue-resource'
-import VueRouter from 'vue-router'
-import App from './App.vue'
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue"
+import "intersection-observer"
+import Vue from "vue"
+import VueResource from "vue-resource"
+import VueRouter from "vue-router"
+import App from "./App.vue"
 import "./filters"
-import ServicePlugin from './plugins/ServicePlugin'
-import routes from './router/index'
-import store from './store/index'
+import ServicePlugin from "./plugins/ServicePlugin"
+import routes from "./router/index"
+import store from "./store/index"
 
-Vue.use(VueResource);
-Vue.use(VueRouter);
-Vue.use(BootstrapVue);
-Vue.use(BootstrapVueIcons);
-Vue.use(ServicePlugin);
-Vue.config.productionTip = true;
-Vue.component('loading-spinner', LoadingSpinner);
+Vue.use(VueResource)
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.use(ServicePlugin)
+Vue.config.productionTip = true
+Vue.component("loading-spinner", LoadingSpinner)
 
-Vue.http.interceptors.push(function () {
-	return function (response) {
-		if (response.status == 401) {
-			location.replace(`${process.env.BASE_URL}api/auth/login?redirectUri=${window.location}`);
-		}
-	};
-});
+Vue.http.interceptors.push(function() {
+  return function(response) {
+    if (response.status == 401) {
+      location.replace(`${import.meta.env.BASE_URL}api/auth/login?redirectUri=${window.location}`)
+    }
+  }
+})
 
-Vue.http.options.root = process.env.BASE_URL;
+Vue.http.options.root = import.meta.env.BASE_URL
 
 // Redirect from / to /jasper/
 if (location.pathname == "/")
-	history.pushState({ page: "home" }, "", process.env.BASE_URL);
+  history.pushState({ page: "home" }, "", import.meta.env.BASE_URL)
 
 const router = new VueRouter({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes: routes
-});
+  mode: "history",
+  base: import.meta.env.BASE_URL,
+  routes: routes
+})
 
 new Vue({
-	router,
-	store,
-	render: h => h(App)
-}).$mount('#app');
+  router,
+  store,
+  render: (h) => h(App)
+}).$mount("#app")
--- web/src/utils/utils.ts
+++ web/src/utils/utils.ts
@@ -1,40 +1,42 @@
-import Vue from 'vue'
-import { Logger as SplunkLogger } from 'splunk-logging';
+import Vue from "vue"
 
 export const SessionManager = {
-    getSettings: async function(store) {
-        try {
-            if (!store.state.userInfo) {
-                const response = await Vue.http.get('api/auth/info');
-                store.commit("CommonInformation/setEnableArchive", response.body.enableArchive);
-                store.commit("CommonInformation/setUserInfo", response.body);
-            }
-            return true;
-        }
-        catch (error) {
-            console.log(error);  
-            return false;
-        }
+  getSettings: async function(store) {
+    try {
+      if (!store.state.userInfo) {
+        const response = await Vue.http.get("api/auth/info")
+        store.commit(
+          "CommonInformation/setEnableArchive",
+          response.body.enableArchive
+        )
+        store.commit("CommonInformation/setUserInfo", response.body)
+      }
+      return true
+    } catch (error) {
+      console.log(error)
+      return false
     }
+  }
 }
 
 export const splunkLog = (message) => {
-    const token = process.env["SPLUNK_TOKEN"] || "";
-    const url = process.env["SPLUNK_COLLECTOR_URL"] || "";
+  console.log(message)
+  // const token = import.meta.env["SPLUNK_TOKEN"] || ""
+  // const url = import.meta.env["SPLUNK_COLLECTOR_URL"] || ""
 
-    if (token && url) {
-        const config = {
-            token: token,
-            url: url
-        };
-    
-        const Logger = new SplunkLogger(config);
-        const payload = {
-            message: message
-        };
-    
-        Logger.send(payload, (err, resp, body)=> {
-            console.log("Response from Splunk", body);
-        });
-    }
-  };
+  // if (token && url) {
+  //   const config = {
+  //     token: token,
+  //     url: url
+  //   }
+
+  //   const Logger = new SplunkLogger(config)
+  //   const payload = {
+  //     message: message
+  //   }
+
+  //   Logger.send(payload, (err, resp, body) => {
+  //     console.log("Response from Splunk", body)
+  //   })
+  // }
+}
