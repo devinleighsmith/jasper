c7dec3855da1c874db8eb06dbeb476794778eee4
Josh Traill <josh.traill@quartech.com>
2024-12-13 10:46:43 -0800

    refactor: remove duplicate import

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -1,9 +1,8 @@
-import '@mdi/font/css/materialdesignicons.css';
+import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import { VBtn } from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 
