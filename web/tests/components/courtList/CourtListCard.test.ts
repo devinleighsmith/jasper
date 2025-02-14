c1fd5cc2a450f3ff0fe7a7780f077143b83d6cd0
Josh Traill <josh.traill@quartech.com>
2025-02-14 13:16:46 -0800

    test: adjust path

--- web/tests/components/courtList/CourtListCard.test.ts
+++ web/tests/components/courtList/CourtListCard.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
-import CourtListCard from 'CMP/courtList/CourtListCard.vue';
+import CourtListCard from 'CMP/courtlist/CourtListCard.vue';
 
 describe('CourtListCard.vue', () => {
   it('renders court list location', () => {
