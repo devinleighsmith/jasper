0dfeb0f68c1ae648d490053f9953693da97478d5
Josh Traill <josh.traill@quartech.com>
2025-03-11 16:39:06 -0700

    style: spacing of top bar

--- web/src/components/case-details/CourtFilesSelector.vue
+++ web/src/components/case-details/CourtFilesSelector.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card color="#72acca" variant="flat">
     <v-container class="pb-0">
-      <v-row>
+      <v-row class="pb-3">
         <v-col cols="10" />
         <v-col>
           <v-btn base-color="white">View all documents</v-btn>
