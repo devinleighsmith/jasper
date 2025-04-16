02d4c23742d761d34d40ba9443a4be6516488be8
Josh Traill <josh.traill@quartech.com>
2025-04-16 10:13:56 -0700

    test: document view tests

--- web/tests/components/case-details/DocumentsView.test.ts
+++ web/tests/components/case-details/DocumentsView.test.ts
@@ -1,14 +1,20 @@
 import { describe, it, expect, beforeEach } from 'vitest';
-import { mount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import { setActivePinia, createPinia } from 'pinia'
+import shared from '@/components/shared';
 import DocumentsView from 'CMP/case-details/DocumentsView.vue';
 
 describe('DocumentsView.vue', () => {
-  let wrapper: ReturnType<typeof mount>;
+  let wrapper: any;
+  let mockParticipantOne: any;
+  let mockParticipantTwo: any;
+  let mockParticipants: any[];
 
-  const mockParticipants = [
-    {
+  beforeEach(() => {
+    mockParticipantOne = {
       fullName: 'John Doe',
+      lastNm: 'Doe',
+      givenNm: 'John',
       profSeqNo: 1,
       document: [
         {
@@ -19,10 +25,12 @@ describe('DocumentsView.vue', () => {
           imageId: '123',
         },
       ],
-    },
-    {
+    };
+    mockParticipantTwo = {
       fullName: 'Jane Smith',
       profSeqNo: 2,
+      lastNm: 'Smith',
+      givenNm: 'Jane',
       document: [
         {
           issueDate: '2023-02-01',
@@ -32,12 +40,13 @@ describe('DocumentsView.vue', () => {
           imageId: '456',
         },
       ],
-    },
-  ];
-
-  beforeEach(() => {
+    };
+    mockParticipants = [
+      mockParticipantOne,
+      mockParticipantTwo
+    ];
     setActivePinia(createPinia());
-    wrapper = mount(DocumentsView, {
+    wrapper = shallowMount(DocumentsView, {
       props: {
         participants: mockParticipants,
       },
@@ -46,7 +55,71 @@ describe('DocumentsView.vue', () => {
 
   it('renders only documents section', () => {
     const sections = wrapper.findAll('v-card-text .text-h5');
+    const tables = wrapper.findAll('v-data-table-virtual');
+
+    expect(tables).toHaveLength(1);
     expect(sections).toHaveLength(1);
-    expect(sections[0].text()).toBe('Documents');
+    expect(sections[0].text()).toContain('All Documents');
+  });
+
+  it('renders correct all documents header with correct count', () => {
+    mockParticipants.push(mockParticipantOne);
+    const wrapper = shallowMount(DocumentsView, {
+      props: {
+        participants: mockParticipants
+      },
+    });
+    const sections = wrapper.findAll('v-card-text .text-h5');
+    expect(sections).toHaveLength(1);
+    expect(sections[0].text()).toBe('All Documents (3)');
+  });
+
+  it('does not render table when no documents', () => {
+    mockParticipants.length = 0;
+    mockParticipantOne.document = [];
+    mockParticipants.push(mockParticipantOne);
+
+    const wrapper = shallowMount(DocumentsView, {
+      props: {
+        participants: mockParticipants
+      },
+    });
+    const sections = wrapper.findAll('v-card-text .text-h5');
+    const tables = wrapper.findAll('v-data-table-virtual');
+    expect(tables).toHaveLength(0);
+    expect(sections).toHaveLength(0);
+  });
+
+  it('computes unfilteredDocuments correctly', async () => {
+    wrapper.vm.selectedCategory = 'ROP';
+
+    const unfilteredDocuments = wrapper.vm.unfilteredDocuments;
+    expect(unfilteredDocuments).toHaveLength(2);
+  });
+
+  it('filters documents by category', async () => {
+    wrapper.vm.selectedCategory = 'ROP';
+    
+    const documents = wrapper.vm.documents;
+    expect(documents).toHaveLength(1);
+    expect(documents[0].category).toBe('rop');
+  });
+
+  it.each([
+    ['rop', 'ROP'],
+    ['other', 'other']
+  ])('formats category correctly', (input, expected) => {
+    const formattedCategory = wrapper.vm.formatCategory({
+      category: input,
+    });
+    expect(formattedCategory).toBe(expected);
+  });
+
+  it.each([
+    [{ category: 'rop', documentTypeDescription: '' }, 'Record of Proceedings'],
+    [{ category: 'other', documentTypeDescription: 'Other' }, 'Other']
+  ])('formats document type correctly for %o', (input, expected) => {
+    const formattedType = wrapper.vm.formatType(input);
+    expect(formattedType).toBe(expected);
   });
 });
