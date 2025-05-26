f726af61db893672458d478c8f00473534809c24
Ronaldo Macapobre <ronaldo.macapobre@quartech.com>
2025-05-26 11:14:49 -0700

    Fix 500 Error (#336)
    * Remove appending of var.env as it is no longer needed in new api gw url
    
    * Upgrade RDS db version to 16.8
    
    ---------
    
    Co-authored-by: Ronaldo Macapobre <rmacapob@gov.bc.ca>

--- infrastructure/cloud/modules/RDS/main.tf
+++ infrastructure/cloud/modules/RDS/main.tf
@@ -12,7 +12,7 @@ resource "aws_db_instance" "postgres_db_instance" {
   allocated_storage                   = 20
   storage_type                        = "gp2"
   engine                              = "postgres"
-  engine_version                      = "16.4"
+  engine_version                      = "16.8"
   instance_class                      = "db.t3.micro"
   db_name                             = "${var.app_name}postgresdb${var.environment}"
   username                            = var.db_username
