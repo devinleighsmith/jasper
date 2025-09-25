c1d2af920f9c82ff0dcf1211402a0bfaa42f74da
JTraill <josh.traill@quartech.com>
2025-09-25 07:49:08 -0700

    refactor: remove endpoint used in testing

--- api/Controllers/ReservedJudgementsController.cs
+++ api/Controllers/ReservedJudgementsController.cs
@@ -1,15 +1,9 @@
-using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
-using MapsterMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Scv.Api.Documents.Parsers;
-using Scv.Api.Documents.Parsers.Models;
 using Scv.Api.Infrastructure.Authorization;
 using Scv.Api.Models;
 using Scv.Api.Services;
-using Scv.Db.Models;
 
 namespace Scv.Api.Controllers;
 
@@ -19,19 +13,6 @@ namespace Scv.Api.Controllers;
 public class ReservedJudgementsController(ICrudService<ReservedJudgementDto> judgementService) : ControllerBase
 {
     private readonly ICrudService<ReservedJudgementDto> _judgementService = judgementService;
-
-    [HttpPost]
-    public async Task<ActionResult> CreateReservedJudgement(ICsvParser parser, IMapper mapper)
-    {
-        var fileBytes = await System.IO.File.ReadAllBytesAsync("files/test.csv");
-        using var memoryStream = new MemoryStream(fileBytes);
-        var records = parser.Parse<CsvReservedJudgement>(memoryStream);
-        var reservedJudgements = mapper.Map<List<ReservedJudgement>>(records);
-        var dtoList = mapper.Map<List<ReservedJudgementDto>>(reservedJudgements);
-        await _judgementService.AddRangeAsync(dtoList);
-
-        return Ok(records);
-    }
 
     [HttpGet]
     public async Task<ActionResult> GetReservedJudgements()
