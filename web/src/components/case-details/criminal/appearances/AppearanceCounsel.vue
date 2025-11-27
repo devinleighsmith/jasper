e50a1d96344b6dd210c0038a4dcd2ad15905fbbc
JTraill <josh.traill@quartech.com>
2025-11-26 16:14:44 -0800

    refactor: merge past and future appearances

--- web/src/components/case-details/criminal/appearances/AppearanceCounsel.vue
+++ web/src/components/case-details/criminal/appearances/AppearanceCounsel.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <span>No counsel.</span>
+    <span v-if="counsel.fullName">
+      {{ counsel.fullName }}
+    </span>
+    <span v-else>No counsel.</span>
   </div>
 </template>
+
+
+<script setup lang="ts">
+  import { JustinCounsel } from '@/types/criminal/jsonTypes';
+
+  defineProps<{
+    counsel: JustinCounsel
+  }>();
+</script>
