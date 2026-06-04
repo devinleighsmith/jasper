63174cd0dc45c223cedcc513e79d477360bd3d1b
Ronaldo Macapobre <ronaldo.macapobre@quartech.com>
2026-06-04 09:49:03 -0700

    Update TF RDS script to match  engine version in AWS (#1113)
    Co-authored-by: Ronaldo Macapobre <ronaldo.macapobre@gov.bc.ca>

--- infrastructure/cloud/modules/RDS/main.tf
+++ infrastructure/cloud/modules/RDS/main.tf
@@ -12,7 +12,7 @@ resource "aws_db_instance" "postgres_db_instance" {
   allocated_storage                   = 20
   storage_type                        = "gp2"
   engine                              = "postgres"
-  engine_version                      = "16.8"
+  engine_version                      = "16.13"
   instance_class                      = "db.t3.micro"
   db_name                             = "${var.app_name}postgresdb${replace(var.environment, "-", "")}"
   username                            = var.db_username
